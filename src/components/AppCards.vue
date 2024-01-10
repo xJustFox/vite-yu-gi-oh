63688c31de57b1527602978c0f5c8b4f8384d4a0
xJustFox <dodydaversa@gmail.com>
2024-01-10 19:10:30 +0100

    [Upgrade] Created Main

---
+++ src/components/AppCards.vue
@@ -0,0 +1,72 @@
+<script>
+import axios, { Axios } from 'axios';
+import { store } from '../store.js';
+export default {
+    name: 'AppCards',
+    data() {
+        return {
+            store
+        }
+    },
+    created() {
+        this.getCardList()
+    },
+    methods: {
+        getCardList(){
+            axios.get(store.endpoint).then((response) => {
+                this.store.cardList = response.data.data
+                console.log(response.data.data);
+            })
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div class="my-col" v-for="(card, index) in store.cardList" :key="index">
+
+        <div class="card text-white" style="background-color: #201f1f;">
+            <img v-for="(img, index) in card.card_images" :key="index" :src="img.image_url" class="card-img-top" alt="...">
+            <div class="card-body card-text-box">
+                
+                <div class="card-text fs-smaller fw-600 ">{{card.name}}</div>
+                <div class="card-text fs-small"><span class="fw-600">Type:</span> {{card.archetype}}</div>
+                <div class="card-text fs-small"><span class="fw-600">Description: <br> </span> 
+                    {{card.desc}}
+                </div>
+
+            </div>
+          </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .my-col{
+        width: calc(100% / 5 - 40px);
+
+        img{
+            width: 100%;
+        }
+        
+        .card-text-box{
+            height: 150px;
+            overflow: overlay;
+            
+            
+            .fw-600{
+                font-weight: 600;
+            }
+            
+            .fs-smaller{
+                font-size: smaller;
+            }
+            .fs-small{
+                font-size: small;
+            }
+        }
+        ::-webkit-scrollbar {
+            display:none;
+        }
+          
+    }
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,70 @@
 <script>
+import AppCards from './AppCards.vue';
+
 export default {
     name: 'AppMain',
+    components: {
+        AppCards,
+    }
 }
 </script>
 
 <template lang="">
-    <div>
-        
-    </div>
+    <main class="py-4">
+        <div class="container test">
+            <div class="my-row">
+                <div class="my-col-12 text-white mx-5">
+                    <form class="row row-cols-lg-auto g-3 align-items-center ">
+                        <div class="col-12">
+                          <div class="input-group input-group-sm">
+                            <input type="text" class="form-control" id="card-name" placeholder="Card name">
+                          </div>
+                        </div>
+                      
+                        <div class="col-12">
+                          <select class="form-select form-select-sm " id="type-cards">
+                            <option selected>Type</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div>
+                      
+                        <div class="col-12">
+                          <button type="submit" class="btn btn-sm btn-primary">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <AppCards />
+            </div>
+        </div>
+    </main>
 </template>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+    main{
+        background-color: #201f1f;
+        
+        .my-row{
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            gap: 20px;
+
+            .my-col-12{
+                width: 100%;
+            }
+        }
+        .test{
+            background-color: $my-black;
+            border-radius: 10px;
+            padding: 20px 0;
+        }
+
+        .bg-light-dark{
+            background-color: #201f1f;
+        }
+    }
 </style>
